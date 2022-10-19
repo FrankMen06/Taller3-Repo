8ca7c3f257cc5bc8c101a166da0ff858400600a5
Frank <100966778+FrankMen06@users.noreply.github.com>
2022-10-19 11:34:19 -0600

    almacenamiento local

--- js/taller.js
+++ js/taller.js
@@ -1,27 +1,29 @@
-const indexedDB = window.indexedDB;
+function guardarDatos(){
+    localStorage.nombre = document.getElementById("nombres").value;
+    localStorage.dui = document.getElementById("dui").value;
+    localStorage.marca = document.getElementById("marca").value;
+    localStorage.modelo = document.getElementById("modelo").value;
+    localStorage.anio = document.getElementById("anio").value;
+    localStorage.color = document.getElementById("color").value;
+    localStorage.placa = document.getElementById("placa").value;
+    localStorage.falla = document.getElementById("falla").value;
 
-if(indexedDB){
-    let db;
+    const fecha = new Date();
 
-    const conexion = indexedDB.open('tallerSpeedy', 1)
-
-    conexion.onsuccess = () =>{
-        db = conexion.result;
-        console.log("La BD está abierta", db);
-    }
-
-    conexion.onupgradeneeded = (e) =>{
-        db = e.target.result
-        console.log("BD creada", db)
-        const coleccionObjetos = db.createObjectStore("Taller", {
-            keyPath: 'clave'
-        })
-    }
-
-    conexion.onerror = (error) =>{
-        console.log("Error", error)
+    if ((localStorage.nombre != undefined) && (localStorage.dui != undefined) && (localStorage.nit != undefined) && (localStorage.marca != undefined) && (localStorage.modelo != undefined) && (localStorage.anio != undefined) && (localStorage.color != undefined) && (localStorage.placa != undefined) && (localStorage.falla != undefined)) {
+        document.getElementById("name").innerHTML = localStorage.nombre ;
+        document.getElementById("dUI").innerHTML = localStorage.dui ;
+        document.getElementById("nIT").innerHTML = localStorage.nit ;
+        document.getElementById("marcA").innerHTML = localStorage.marca ;
+        document.getElementById("modeLO").innerHTML = localStorage.modelo ;
+        document.getElementById("aniO").innerHTML = localStorage.anio ;
+        document.getElementById("colOR").innerHTML = localStorage.color ;
+        document.getElementById("plaCA").innerHTML = localStorage.placa ;
+        document.getElementById("faLLA").innerHTML = localStorage.falla ;
+        document.getElementById("ingreso").innerHTML = fecha; 
+        
+    } else {
+        document.getElementById("datos").innerHTML = "No hay Datos";
     }
 
 }
-
-
