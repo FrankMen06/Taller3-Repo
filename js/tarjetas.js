082ee8b62223def7188177bb0df17dbe9a9fd882
Frank <100966778+FrankMen06@users.noreply.github.com>
2022-10-27 15:16:58 -0600

    Update tarjetas.js

--- js/tarjetas.js
+++ js/tarjetas.js
@@ -1,1 +1,34 @@
-let nombres;
+function enviar(){
+
+    localStorage.nombre = document.getElementById("nombres").value;
+    localStorage.tipo = document.getElementById("tipo").value;
+    localStorage.fechCad = document.getElementById("fechCad").value;
+    localStorage.monto = document.getElementById("monto").value;
+    localStorage.fechCort = document.getElementById("fechCort").value;
+
+
+    var name = document.getElementById("nombre");
+    var tip = document.getElementById("tip");
+    var cad = document.getElementById("cad");
+    var mont = document.getElementById("mont");
+    var cort = document.getElementById("cort");
+
+
+    name.innerHTML = nombre;
+    tip.innerHTML = tipo;
+    cad.innerHTML = fechCad;
+    mont.innerHTML = monto;
+    cort.innerHTML = cort;
+    
+
+
+
+   /* if(nombre == "" || tipo == "" || fechCad == "" || monto == "" ||  fechCort == "") {
+
+        alert("Rellene los espacios en blanco");
+
+
+    }else{
+
+    }*/
+}
